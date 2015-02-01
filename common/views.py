299d3d6cfc21a96684d774f6dc7e7099d663d43f
alff0x1f <alff0x1f@gmail.com>
2015-02-01 18:43:44 +0000

    Add basic template

--- common/views.py
+++ common/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    user_id = request.COOKIES.get('phpbb3_8ioea_u')
+    return render(request, 'common/home.html', {'user_id': user_id})
